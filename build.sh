6eab604a8a7db04772ef2a1e5396b33289bbbfff
Birendra-dev <077bct025@ioepc.edu.np>
2025-05-20 23:07:10 +0545

    editing build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Create static directories if they don't exist
+mkdir -p static
+mkdir -p blog/static/blog
+
+# Run collectstatic with clear flag to ensure all files are collected
+python manage.py collectstatic --noinput --clear
+
+# Run migrations
 python manage.py migrate
